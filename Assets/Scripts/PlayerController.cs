a630fb45f9f2dd21023f3ce5222ddcb2677940af
Christian Mendoza <christianmendozater@gmail.com>
2023-09-24 23:26:48 -0400

    new controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,46 +1,36 @@
 using System.Collections;
-using System.Collections.Generic; 
+using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
 
-  [SerializeField] float moveSpeed = 5f;
-  
-  Rigidbody2D rb;
-  Vector3 mousePosition, whereToMove;
-  
-  bool isMoving = false;
-  float previousDistanceToMousePos, currentDistanceToMousePos;
+    Rigidbody2D rb;
+    Vector3 mousePosition, whereToMove;
 
-  void Start() {
-    rb = GetComponent<Rigidbody2D>(); 
-  }
+    bool isMoving = false;
 
-  void Update() {
-
-    if (isMoving) 
-      currentDistanceToMousePos = (mousePosition - transform.position).magnitude;
-
-    if (Input.GetMouseButtonDown(0)) {
-      previousDistanceToMousePos = 0;
-      currentDistanceToMousePos = 0;
-      isMoving = true;
-      
-      mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-      mousePosition.z = 0f;
-      
-      whereToMove = (mousePosition - transform.position).normalized;
-      rb.velocity = new Vector2(whereToMove.x * moveSpeed, whereToMove.y * moveSpeed);
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    if (currentDistanceToMousePos > previousDistanceToMousePos) {
-      isMoving = false;
-      rb.velocity = Vector2.zero; 
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isMoving = true;
+
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0f;
+
+            whereToMove = (mousePosition - transform.position).normalized;
+        }
+
+        if (isMoving)
+        {
+            rb.velocity = new Vector2(whereToMove.x * moveSpeed, whereToMove.y * moveSpeed);
+        }
     }
-
-    if (isMoving) 
-      previousDistanceToMousePos = (mousePosition - transform.position).magnitude;
-
-  }
-
 }
