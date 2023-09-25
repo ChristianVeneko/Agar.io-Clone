a5e2b90dad590ff8e557781e40b3e029966a1ac9
Christian Mendoza <christianmendozater@gmail.com>
2023-09-24 23:27:13 -0400

    Player can Increase their size

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,11 @@ public class Player : MonoBehaviour
 {
     public static Player instance;
     public static Player Instance { get { return instance; } }
+
+    public float scaleThreshold = 0.5f; // El umbral de escala en el que la cámara se alejará.
+
+    private Camera mainCamera;
+
     void Awake()
     {
         if (instance == null)
@@ -18,7 +23,28 @@ public class Player : MonoBehaviour
         }
     }
 
-    public void IncreaseSize(float size){
-        gameObject.transform.localScale += new Vector3(size, size, 0f);
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    public void IncreaseSize(float size)
+    {
+        Transform playerTransform = gameObject.transform;
+        // Aumentar la escala del jugador.
+        playerTransform.localScale += new Vector3(size, size, 0f);
+
+        // Calcular el tamaño del jugador en la vista de la cámara.
+        float playerScaleInView = playerTransform.lossyScale.x / mainCamera.orthographicSize;
+
+        Debug.Log(playerScaleInView);
+        Debug.Log(scaleThreshold);
+        // Comprobar si el jugador ocupa más del umbral de la pantalla.
+        if (playerScaleInView > scaleThreshold)
+        {
+            Debug.Log(playerScaleInView);
+            Debug.Log("Aumentar la vista de la cámara.");
+            mainCamera.orthographicSize += 0.5f; // Puedes ajustar este valor según tu necesidad.
+        }
     }
 }
